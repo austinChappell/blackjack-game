7fc364984489ff49f143bff3a828c53e8f8a17cc
austinChappell <Austin.W.Chappell@gmail.com>
2017-07-23 21:47:33 -0500

    add comments

--- starter_files/scripts/main.js
+++ starter_files/scripts/main.js
@@ -8,6 +8,8 @@
    you'll need to parse through that first before you can start to
    write your logic.
 */
+
+// Check to see if a card is a face card
 function isFaceCard(card) {
   if (card === 'J' || card === 'Q' || card === 'K') {
     return true;
@@ -16,6 +18,7 @@ function isFaceCard(card) {
   };
 };
 
+// Returns an array of all the possible sums of ace cards
 function aceOptions(num) {
   let optionsArr = [];
   for (let i = 0; i <= num; i++) {
@@ -25,11 +28,13 @@ function aceOptions(num) {
   return optionsArr;
 }
 
+// Total the value of your hand
 function handValue(hand) {
   let subTotal = 0;
   let total = 0;
   let numOfAces = 0;
 
+  // If facecard, add 10, else if not ace, add the number, else add one to number of aces
   for (let i = 0; i < hand.length; i++) {
     if( isFaceCard(hand[i]) ) {
       subTotal += 10;
@@ -40,10 +45,13 @@ function handValue(hand) {
     }
   }
 
+  // returns an array of all possible ace sums
   let aceTotals = aceOptions(numOfAces);
 
+  // equals lowest possible total
   total = subTotal + aceTotals[0];
 
+  // check to see if it is under 21. will return the highest possible total without going over 21
   for (let i = 1; i < aceTotals.length; i++) {
     if (subTotal + aceTotals[i] <= 21) {
       total = subTotal + aceTotals[i];
